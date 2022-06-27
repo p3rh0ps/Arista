02a6fc23d8eca59cbf69e3aa5deec7ca03997439
p3rh0ps <33998785+p3rh0ps@users.noreply.github.com>
2022-06-27 14:35:32 +0200

    Create deploy-sshkeys.py

---
+++ deploy-sshkeys.py
@@ -0,0 +1,18 @@
+import pyeapi
+
+with open('inventory', 'r') as inv_file:
+    host = inv_file.readline().strip('\n')
+    while host:
+        print("*"*20)
+        print("HOSTNAME:", host)
+        print("*"*20)
+        conn = pyeapi.connect_to(host)
+        res_key = conn.api('users').set_sshkey(name='arista',
+        value='ssh-rsa AAAAPLACEYOUROWNKEYHERE\
+HELLLLLLOOOOOTHERECHANGEWITHYOURKEYHEREEEEEEEEEE\
+HELLLLLLOOOOOTHERECHANGEWITHYOURKEYHEREEEEEEEEEE\
+HELLLLLLOOOOOTHERECHANGEWITHYOURKEYHEREEEEEEEEEE\
+HELLLLLLOOOOOTHERECHANGEWITHYOURKEYHEREEEEEEEEEE user@mydom',
+        default=False, disable=False)
+        print(conn.api('users').get('arista'))   # Take a look at the result
+        # print(res_key)                         # Check if you made a change True YES False No
