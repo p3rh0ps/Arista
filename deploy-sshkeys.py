122056a28e8edc55bb10ac76ac0e2f7151581871
p3rh0ps <33998785+p3rh0ps@users.noreply.github.com>
2022-07-05 17:26:36 +0200

    Huge update !
    Add tabulate for clear presentation of the result and catch small error for the python script

--- deploy-sshkeys.py
+++ deploy-sshkeys.py
@@ -1,18 +1,52 @@
+import os
+import sys
+import re
 import pyeapi
+from tabulate import tabulate
+
+
+def get_ssh_infos(ssh_key: str) -> str:
+    ssh_key_type = ssh_key.split(" ")[0]
+    ssh_key_misc = ssh_key.split(" ")[2]
+    if ssh_key_type == "ssh-rsa" and ssh_key_misc.startswith(KEY_USER+"@") and re.match(KEY_USER+"@"+KEY_HOST, ssh_key_misc):
+        return ssh_key_type + " " + ssh_key_misc
+    else:
+        print("You don't use a key generated with the local user and local host of this system, or it is the wrong key please correct")
+        sys.exit(-1)
+
+def ssh_key_set(operator: bool) -> str:
+    print(operator)
+    if operator is True:
+        return "Modified"
+    else:
+        return "Unmodified"
 
 with open('/home/coder/.ssh/id_rsa.pub', 'r') as sshkey:
     SSH_KEY = sshkey.readline()
 
-with open('inventory', 'r') as inv_file:
-    host = inv_file.readline().strip('\n')
-    while host:
-        print("*"*20)
-        print("HOSTNAME:", host)
-        print("*"*20)
-        conn = pyeapi.connect_to(host)
-        res_key = conn.api('users').set_sshkey(name='arista',
-        value=SSH_KEY,
-        default=False, disable=False)
-        print(conn.api('users').get('arista'))
-        print(res_key)
+USER = 'arista'
+KEY_USER = os.getenv('USER')
+KEY_HOST = os.getenv('HOSTNAME')
+fancytab = []
+
+try:
+    with open('inventory', 'r') as inv_file:
         host = inv_file.readline().strip('\n')
+        while host:
+            conn = pyeapi.connect_to(host)
+            key_status = "Unchanged"
+            if conn.api('users').get(USER)['ssh-key'] != SSH_KEY.strip("\n"):
+                res_key_op = conn.api('users').set_sshkey(name=USER,
+                value=SSH_KEY,
+                default=False, disable=False)
+                if res_key_op is False:
+                    raise AssertionError("Issue with ssh key update for host {}".format(host))
+                key_status = "Updated"
+            fancytab.append([host, USER, conn.api('users').get(USER)['privilege'], conn.api('users').get(USER)['role'], get_ssh_infos(conn.api('users').get(USER)['ssh-key']), key_status])
+            host = inv_file.readline().strip('\n')
+except AssertionError:
+    sys.exit(-1)
+except:
+    print("Unknown but catched error :)")
+
+print(tabulate(fancytab, headers=["Hostname", "User", "Privilege", "Role", "Ssh Type and Key", "Ssh-Key-Status"], tablefmt="grid", stralign='center', numalign='center', showindex='always'))
