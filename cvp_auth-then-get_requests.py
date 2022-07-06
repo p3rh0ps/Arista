e30e14f4cefb9ed9886dbe7cc86fbc74a7ad4dd2
p3rh0ps <33998785+p3rh0ps@users.noreply.github.com>
2022-07-06 13:39:47 +0200

    Update cvp_auth-then-get_requests.py

--- cvp_auth-then-get_requests.py
+++ cvp_auth-then-get_requests.py
@@ -1,14 +1,13 @@
 import requests
 import json
 from pprint import pprint
-
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
-CVP_HOST = "*********"
-CVP_USER = "*********"
-CVP_PASS = "*********"
+CVP_HOST = "192.168.0.5"
+CVP_USER = "arista"
+CVP_PASS = "arista5jad"
 
 url = f"https://{CVP_HOST}"
 headers = { 'Content-Type': 'application/json' }
@@ -32,4 +31,8 @@ full_invent = {'inventory': 'false'}
 url = f"https://{CVP_HOST}/cvpservice/inventory/devices"
 invent_response = requests.get(url, cookies=cookies, headers=headers, verify=False, params=full_invent)
 
-pprint(invent_response.json(), indent=4)
+pprint(invent_response.json(), indent=2)
+
+print("Invoked URL:{}".format(invent_response.request.url))
+print(invent_response.request.body) # With POST method interesting with GET no
+pprint(dict(invent_response.request.headers), indent=2)
