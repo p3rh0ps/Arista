68c46f2ace8f8b49456d8b7adde78bc41032c8ac
p3rh0ps <33998785+p3rh0ps@users.noreply.github.com>
2022-07-06 09:39:58 +0200

    Add errors handling, enjoy

--- deploy-sshkeys.py
+++ deploy-sshkeys.py
@@ -11,7 +11,8 @@ def get_ssh_infos(ssh_key: str) -> str:
     if ssh_key_type == "ssh-rsa" and ssh_key_misc.startswith(KEY_USER+"@") and re.match(KEY_USER+"@"+KEY_HOST, ssh_key_misc):
         return ssh_key_type + " " + ssh_key_misc
     else:
-        print("You don't use a key generated with the local user and local host of this system, or it is the wrong key please correct")
+        print("You don't use a key generated with the local user and local host of this system, or it is the wrong key\
+            please correct by creating a new one if necessary via 'ssh-keygen -t rsa' linux cli")
         sys.exit(-1)
 
 def ssh_key_set(operator: bool) -> str:
@@ -44,9 +45,15 @@ try:
                 key_status = "Updated"
             fancytab.append([host, USER, conn.api('users').get(USER)['privilege'], conn.api('users').get(USER)['role'], get_ssh_infos(conn.api('users').get(USER)['ssh-key']), key_status])
             host = inv_file.readline().strip('\n')
-except AssertionError:
+except FileNotFoundError as filenotf_err:
+    print("File not Found: {}".format(filenotf_err.args))
+except AssertionError as assert_err:
+    print("Assertion Error: {}".format(assert_err.args))
     sys.exit(-1)
+except KeyboardInterrupt as keyb_err:
+    print("User Interrupt: {}".format(keyb_err.args))
+    sys.exit(0)
 except:
-    print("Unknown but catched error :)")
+    print("Unknown but catched error")
 
 print(tabulate(fancytab, headers=["Hostname", "User", "Privilege", "Role", "Ssh Type and Key", "Ssh-Key-Status"], tablefmt="grid", stralign='center', numalign='center', showindex='always'))
